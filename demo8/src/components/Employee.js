5abb8ee8551769a3b96eab15ea60ea2f4b74266e
Prashant <prashantbhat94@gmail.com>
2022-11-11 22:38:54 +0530

    "React Router Files Exercises"

--- demo8/src/components/Employee.js
+++ demo8/src/components/Employee.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Navigate} from "react-router-dom";
 
+
 class Employee extends React.Component{
     state = {
         achievements: null,
--- demo8/src/components/EmployeeEdit.js
+++ demo8/src/components/EmployeeEdit.js
@@ -1,13 +1,11 @@
 import React from "react"
 import { useParams } from 'react-router-dom';
 
-
-class EditEmployee extends React.Component{   
-    render = () => {
-        return(
-            <h3>The selected ID is: {this.props.match.params.id}</h3>
-        );
-    }
+function EditEmployee(){   
+    const {id} = useParams();    
+    return(
+        <h3>The selected ID is: {id}</h3>
+    );
 }
 
 export default EditEmployee;
